d06f1b481ef7f68f1abce77759e6ac23c75ccff3
Toshiyuki Oshima <toshiyuki67026@gmail.com>
2022-03-27 21:34:18 +0900

    add pyproject.toml

--- joy_control_sample_ros2/agent_body.py
+++ joy_control_sample_ros2/agent_body.py
@@ -22,29 +22,21 @@ class AgentBody(Node):
     def __init__(self) -> None:
         super().__init__("AgentBody")
 
-        self.cmd_vel_sub = self.create_subscription(
-            Twist, "cmd_vel", self.cmd_vel_callback, 10
-        )
+        self.cmd_vel_sub = self.create_subscription(Twist, "cmd_vel", self.cmd_vel_callback, 10)
         self.curr_pose_pub = self.create_publisher(Pose, "curr_pose", 10)
         self.curr_pose = Pose()
 
         # get frame name
         self.declare_parameter("world_frame", "world")
-        self.world_frame = (
-            self.get_parameter("world_frame").get_parameter_value().string_value
-        )
+        self.world_frame = self.get_parameter("world_frame").get_parameter_value().string_value
         self.declare_parameter("agent_frame", "agent")
-        self.agent_frame = (
-            self.get_parameter("agent_frame").get_parameter_value().string_value
-        )
+        self.agent_frame = self.get_parameter("agent_frame").get_parameter_value().string_value
 
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
         self.broadcaster = TransformBroadcaster(self)
         self.declare_parameter("timer_period", 0.01)
-        self.timer_period = (
-            self.get_parameter("timer_period").get_parameter_value().double_value
-        )
+        self.timer_period = self.get_parameter("timer_period").get_parameter_value().double_value
 
     def cmd_vel_callback(self, msg: Twist) -> None:
         position = self.curr_pose.position
@@ -56,18 +48,12 @@ class AgentBody(Node):
         quat = self.curr_pose.orientation
         _, _, yaw = euler_from_quaternion(quaternion=[quat.x, quat.y, quat.z, quat.w])
 
-        quat = quaternion_from_euler(
-            ai=0, aj=0, ak=yaw + self.timer_period * msg.angular.z
-        )
-        self.curr_pose.orientation = Quaternion(
-            x=quat[0], y=quat[1], z=quat[2], w=quat[3]
-        )
+        quat = quaternion_from_euler(ai=0, aj=0, ak=yaw + self.timer_period * msg.angular.z)
+        self.curr_pose.orientation = Quaternion(x=quat[0], y=quat[1], z=quat[2], w=quat[3])
         self.curr_pose_pub.publish(self.curr_pose)
 
         transform_stamped = TransformStamped(
-            header=Header(
-                stamp=self.get_clock().now().to_msg(), frame_id=self.world_frame
-            ),
+            header=Header(stamp=self.get_clock().now().to_msg(), frame_id=self.world_frame),
             child_frame_id=self.agent_frame,
             transform=Transform(
                 translation=Vector3(
